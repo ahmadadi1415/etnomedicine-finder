67c41b91e552bfd0b98d6dc6c0c318ba10a37f21
ahmadadi1415 <adipras606@gmail.com>
2023-05-10 19:07:16 +0700

    enable cors

--- next.config.js
+++ next.config.js
@@ -6,4 +6,19 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
